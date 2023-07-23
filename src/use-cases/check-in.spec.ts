fb8fe04f386e696e4f214dc4fdff43fa6db735dc
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2023-07-23 11:02:10 -0300

    feat: TDD

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -19,4 +19,16 @@ beforeEach(() => {
     
         expect(checkIn.id).toEqual(expect.any(String))
     })
+
+    it('should not be able to check in twice in the same day', async () => {
+        await sut.execute({
+            gymId: 'gym-01',
+            userId: 'user-01',
+        })
+        
+        expect(() => sut.execute({
+            gymId: 'gym-01',
+            userId: 'user-01',
+        })).rejects.toBeInstanceOf(Error)
+    })
 })
