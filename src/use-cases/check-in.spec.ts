aa81cd133f550ac2d322e3409d61e556e7abaf4e
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2023-07-23 11:12:20 -0300

    tdd test

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, afterEach, vi } from 'vitest'
 import { CheckInUseCase } from './check-in'
 import { InMemoryCheckInsRepository } from '../repositories/in-memory-user-repository/in-memory-check-ins-repository'
 
@@ -6,10 +6,16 @@ let checkInsRepository: InMemoryCheckInsRepository
 let sut: CheckInUseCase
 
 describe('Check-in Use Case', () => {
-beforeEach(() => {
+    beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
 })
+
+    afterEach(() => {
+        vi.useRealTimers()
+    })
 
     it('should be able to check in', async () => {
         const { checkIn } = await sut.execute({
@@ -21,12 +27,14 @@ beforeEach(() => {
     })
 
     it('should not be able to check in twice in the same day', async () => {
+        vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
         await sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
         })
         
-        expect(() => sut.execute({
+        await expect(() => sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
         })).rejects.toBeInstanceOf(Error)
