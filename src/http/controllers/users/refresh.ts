3953656c395aae3ed77017813ba67ae77639ab08
shimarrudz <vic.shima.vss@gmail.com>
2023-07-29 12:54:49 -0300

    feat: creating user controller

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,36 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify({ onlyCookie: true })
+
+  const { role, sub } = request.user
+
+  const token = await reply.jwtSign(
+    { role },
+    {
+      sign: {
+        sub,
+      },
+    },
+  )
+
+  const refreshToken = await reply.jwtSign(
+    { role },
+    {
+      sign: {
+        sub,
+        expiresIn: '7d',
+      },
+    },
+  )
+
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({ token })
+}
