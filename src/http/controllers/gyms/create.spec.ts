23171d7b1aefb23a17481212c850e0e77be0f8d2
shimarrudz <vic.shima.vss@gmail.com>
2023-07-29 12:51:08 -0300

    feat: creating gyms controller

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app, true)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description.',
+        phone: '11999999999',
+        latitude: -27.0747279,
+        longitude: -49.4889672,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
---
+++ src/http/controllers/gyms/routes.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance } from 'fastify'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
+import { create } from './create'
+import { nearby } from './nearby'
+import { search } from './search'
+
+export async function gymsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+}
