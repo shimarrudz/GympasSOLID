2d1e35eba255c75d561825bda3b05be86043a5a2
shimarrudz <vic.shima.vss@gmail.com>
2023-07-29 13:00:33 -0300

    refactor: refactoring errors

---
+++ src/use-cases/errors/late-check-in-error.ts
@@ -0,0 +1,8 @@
+export class LateCheckInError extends Error {
+    constructor() {
+      super(
+        'The check-in can only be validated until 20 minutes of its creation.',
+      )
+    }
+  }
+  
