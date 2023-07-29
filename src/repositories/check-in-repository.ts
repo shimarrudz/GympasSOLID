4282d7e2384846ca5e73f934382dbebb691ad559
shimarrudz <vic.shima.vss@gmail.com>
2023-07-29 12:59:33 -0300

    refactor: refactoring repositories interface

--- src/repositories/check-in-repository.ts
+++ src/repositories/check-in-repository.ts
@@ -1,6 +1,10 @@
-import { CheckIn, Prisma } from "@prisma/client";
+import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
-    create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
-    findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  save(checkIn: CheckIn): Promise<CheckIn>
+  countByUserId(userId: string): Promise<number>
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  findById(id: string): Promise<CheckIn | null>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,6 +1,13 @@
-import { Gym, Prisma } from "@prisma/client";
+import { Gym, Prisma } from '@prisma/client'
+
+export interface FindManyNearbyParams {
+  latitude: number
+  longitude: number
+}
 
 export interface GymsRepository {
-    findById(id: string): Promise<Gym | null>
-    create(data: Prisma.GymCreateInput): Promise<Gym>
+  create(data: Prisma.GymCreateInput): Promise<Gym>
+  findById(id: string): Promise<Gym | null>
+  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
+  searchMany(query: string, page: number): Promise<Gym[]>
 }
